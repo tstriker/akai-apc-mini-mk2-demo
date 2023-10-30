d930a91d15ecae0bf048fd554db05fe97ceba3da
Tom Striker <toms.baugis@gmail.com>
2023-10-30 00:39:57 +0000

    beginnings of event handlers and sprites and whatnot (why oh why)

--- src/Palette.vue
+++ src/Palette.vue
@@ -7,6 +7,247 @@
     import utils from "./utils.js";
     import APC from "./APC.vue";
 
+    let pixelKey = (x, y) => `${x},${y}`;
+
+    class Sprite {
+        constructor(x, y) {
+            [this.x, this.y] = [x, y];
+        }
+
+        getPixels() {
+            throw Error("define me!");
+        }
+    }
+
+    class Rect extends Sprite {
+        constructor(x, y, width, height, color) {
+            super(x, y);
+            [this.width, this.height, this.color] = [width, height, color];
+        }
+
+        getPixels() {
+            let pixels = {};
+            for (let y = this.y; y < this.y + this.height; y++) {
+                for (let x = this.x; x < this.x + this.width; x++) {
+                    pixels[pixelKey(x, y)] = this.color;
+                }
+            }
+            return pixels;
+        }
+    }
+
+    // letters, vertical line by line in base 36
+    // `parseInt("01110", 2).toString(36)` --> 'e'
+    let letters = {
+        A: "fkf",
+        B: "vla",
+        C: "vhh",
+        D: "vhe",
+        E: "vl",
+        F: "vkg",
+        G: "vhl7",
+        H: "v4v",
+        I: "v",
+        J: "3hv",
+        K: "v4r",
+        L: "v1",
+        M: "v848v",
+        N: "vgf",
+        O: "ehe",
+        P: "vkks",
+        Q: "uiju",
+        R: "vkb",
+        S: "tln",
+        T: "gvg",
+        U: "v1v",
+        V: "u1u",
+        W: "s343s",
+        X: "r4r",
+        Y: "o7o",
+        Z: "jip",
+        0: "ehe",
+        1: "8v",
+        2: "nt",
+        3: "lv",
+        4: "s4v",
+        5: "tn",
+        6: "vln",
+        7: "gno",
+        8: "vlv",
+        9: "tlv",
+        "!": "t",
+        "?": "gl8",
+        "\\": "o43",
+        '"': "o0o",
+        "#": "lvlvl",
+        "&": "",
+        "(": "eh",
+        ")": "he",
+        "[": "vh",
+        "]": "hv",
+        "-": "44",
+        "+": "4e4",
+        "*": "a4a",
+        ":": "a",
+        "'": "o",
+    };
+
+    class Text extends Sprite {
+        constructor(x, y, text, color = "#ffffff") {
+            super(x, y);
+            [this.text, this.color] = [text, color];
+
+            this.height = 5;
+            this.width = 0;
+            this._redraw();
+        }
+
+        _redraw() {
+            let [x, y] = [0, 0];
+            let pixels = {};
+            for (let letter of this.text.toUpperCase()) {
+                let lines = letters[letter] || [];
+                for (let line of lines) {
+                    let bytes = parseInt(line, 36).toString(2).padStart(5, "0");
+                    for (let byte of bytes) {
+                        if (byte != "0") {
+                            pixels[pixelKey(x, y)] = this.color;
+                        }
+                        y += 1;
+                    }
+                    x += 1;
+                    y = 0;
+                }
+
+                // add whitespace after each letter because we are polite
+                x += 1;
+            }
+            this._pixels = pixels;
+            this.width = x;
+        }
+
+        getPixels() {
+            return this._pixels;
+        }
+    }
+
+    class PixelCanvas {
+        constructor({x, y, width, height, bg}) {
+            this.x = x || 0;
+            this.y = y || 0;
+            this.width = width || 8;
+            this.height = height || 8;
+            this.bg = bg || "#000000";
+            this.sprites = [];
+        }
+
+        addChild(sprite) {
+            this.sprites.push(sprite);
+        }
+
+        getPixels() {
+            // gets colors for all sprites and then returns the full canvas
+            // we do not do transparency as we need to clean up
+            let pixelBoard = {};
+            for (let x = 0; x < this.width; x++) {
+                pixelBoard[x] = {};
+            }
+
+            for (let sprite of this.sprites) {
+                Object.entries(sprite.getPixels()).forEach(([key, color]) => {
+                    let [x, y] = key.split(",");
+                    [x, y] = [Math.round(parseInt(x) + sprite.x), Math.round(parseInt(y) + sprite.y)];
+                    if (x >= 0 && x < this.width && y >= 0 && y < this.height) {
+                        pixelBoard[x][y] = color;
+                    }
+                });
+            }
+
+            let pixels = [];
+            for (let y = 0; y < this.height; y++) {
+                for (let x = 0; x < this.width; x++) {
+                    pixels.push({x: x + this.x, y: y + this.y, color: pixelBoard[x][y] || this.bg});
+                }
+            }
+            pixels = pixels.filter(p => p.x >= 0 && p.x < 8 && p.y >= 0 && p.y <= 7);
+            return pixels;
+        }
+    }
+
+    // this obviously works - finish it after you're done with the canvas
+    class APCHandler {
+        constructor(states, currentStateName) {
+            this.states = states;
+            this.currentState = currentStateName;
+            this.mk2 = null;
+        }
+
+        attach(mk2) {
+            this.mk2 = mk2;
+            this.onEvt = this.onEvt.bind(this);
+            document.addEventListener("noteon", this.onEvt);
+            document.addEventListener("noteoff", this.onEvt);
+            document.addEventListener("cc", this.onEvt);
+        }
+
+        detach() {
+            document.removeEventListener("noteon", this.onEvt);
+            document.removeEventListener("noteoff", this.onEvt);
+            document.removeEventListener("cc", this.onEvt);
+        }
+
+        go(stateName) {
+            this.currentState = this.states[stateName];
+        }
+
+        onEvt(evt) {
+            if (evt.controller != this.mk2 || !this.currentState) {
+                // wrong controller or we don't have a current state - go home
+                return;
+            }
+
+            let {key, type} = evt;
+            let state = this.currentState;
+
+            if (state.handlers[type]) {
+                Object.bind(this, state.handlers[type]);
+                state.handlers[type](evt);
+            }
+
+            if (type == "noteon" && state.handlers[key]) {
+                Object.bind(this, state.handlers[key]);
+                state.handlers[key](evt);
+            }
+        }
+    }
+
+    // let's describe different states
+    let mk2Handler = new APCHandler({
+        paletteBrowser: {
+            handlers: {
+                noteon(evt) {
+                    // forwarded for all events
+                    console.log("eeeeeeeeeee i'm a note on");
+                },
+
+                leftButton() {
+                    // a shorthand for buttonLeft.noteOn
+                    console.log("eeeeeeeeeee i'm a button left");
+                    this.paletteCursor -= 1;
+                },
+
+                rightButton() {
+                    // a shorthand for buttonRight.noteOn
+                    this.paletteCursor += 1;
+                },
+            },
+
+            getPixelStates() {
+                // here we generate everything that needs generating
+            },
+        },
+    });
+
     let palettes = {
         vibrant: [21, 53, 77, 113, 96, 45, 64, 5],
         shades: {
@@ -189,54 +430,54 @@
                 }
             },
 
-            async sendColors() {
-                let lightness = 0.5;
-                let direction = -0.005;
-                let hue = 0;
+            handleAPC(evt) {
+                if (evt.key == "volumeButton") {
+                    this.mk2.reset();
+                } else if (evt.button.type == "rgb") {
+                    evt.button.color = evt.button.color == "#ffffff" ? 0 : "#ffffff";
+                } else if (evt.key == "panButton") {
+                    if (this.canvas.x == 4) {
+                        this.canvas.x = 0;
+                        this.canvas.width = 8;
+                        this.mk2.panButton.toggled = true;
+                    } else {
+                        this.canvas.x = 4;
+                        this.canvas.width = 4;
+                        this.mk2.panButton.toggled = false;
 
-                let a = 0;
-
-                let inner = async () => {
-                    let steps = 64;
-                    for (let idx = 0; idx < steps; idx++) {
-                        let x = idx % 8;
-                        let y = 7 - Math.trunc(idx / 8);
-
-                        let intensity = ((x + y + a) % 14) / 14; // ((x + a) % 64) / 64;
-
-                        let maxHue = 360 - 360 / (steps + 1);
-
-                        let color = chroma.hsl(intensity * maxHue, 1, lightness);
-                        //let color = chroma.hsl(hue, 1, intensity * 0.5);
-
-                        this.mk2.buttons[idx] = color.hex();
+                        this.mk2.select(0, 2, 3, 7).forEach(pad => {
+                            pad.color = 0;
+                        });
                     }
-
-                    // lightness += direction;
-                    // if (lightness < 0.1 || lightness > 0.5) {
-                    //     lightness = Math.min(Math.max(lightness, 0), 0.5);
-                    //     direction = -direction;
-                    // }
-                    hue = (hue + 0.1) % 360;
-
-                    a = a + 0.05;
-
-                    setTimeout(inner, 0);
-                };
-
-                inner();
+                }
             },
         },
 
         async mounted() {
-            this.mk2 = new APCMiniMk2();
-            await this.mk2.connect({sysex: true});
+            this.canvas = new PixelCanvas({x: 4, y: 1, width: 4, height: 7, bg: "#110011"});
+            let message = "[?]??what???";
+            //message = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~";
+            this.label = new Text(4, 1, message);
+            this.canvas.addChild(this.label);
 
-            document.addEventListener("noteon", evt => {
-                if (evt.key == "volume") {
-                    this.mk2.reset();
-                }
+            this.mk2 = new APCMiniMk2();
+            await this.mk2.connect({
+                sysex: true,
+                beforePaint: () => {
+                    this.label.x -= 0.1;
+                    if (-this.label.x > this.label.width) {
+                        this.label.x = 8;
+                    }
+                    for (let pixel of this.canvas.getPixels()) {
+                        this.mk2.pads(pixel.x, pixel.y).color = pixel.color;
+                    }
+                },
             });
+
+            mk2Handler.attach(this.mk2);
+            mk2Handler.go("paletteBrowser");
+
+            document.addEventListener("noteon", this.handleAPC);
 
             Object.values(palettes.shades).forEach((row, idx) => {
                 this.fillColors(7 - idx, 0, row, true);
@@ -249,6 +490,10 @@
 
         beforeUnmount() {
             this.mk2.disconnect();
+
+            mk2Handler.detach();
+
+            document.removeEventListener("noteon", this.handleAPC);
             document.removeEventListener("keydown", this.onDocumentKeyDown);
         },
     };
