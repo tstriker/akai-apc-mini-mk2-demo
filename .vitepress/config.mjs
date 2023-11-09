22128bfdd8d3be391ca011551c2374a71e1202c7
Tom Striker <toms.baugis@gmail.com>
2023-11-09 13:53:43 +0000

    build - shove the output into my github page

--- .vitepress/config.mjs
+++ .vitepress/config.mjs
@@ -5,4 +5,8 @@ export default defineConfig({
     title: "APC Mini Mk2 Demo",
     description: "AKAI APC Mini Mk2 Demo",
     srcDir: "./pages",
+    base: "/apc-mini-mk2",
+
+    // because i'm lazy, showing this lot into my github.io page
+    outDir: "../tstriker.github.io/apc-mini-mk2",
 });
